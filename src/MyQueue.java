8aefc3725cd02136de0f412513c1982a95233736
Magomed2023 <m.muzhakhoev@gmx.at>
2023-07-14 10:29:28 +0200

    Abgabe

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,48 +1,89 @@
 public class MyQueue {
-    private final int SIZE;
-    private int[] queue;
-    private int front, rear;
 
-    public MyQueue(int SIZE) {
-        this.SIZE = SIZE;
-        queue = new int[SIZE];
-        //front = ...
-        //rear = ...
+    int size = 5;
+    int items[] = new int[size];
+    int front;
+    int rear;
+
+    MyQueue() {
+        front = -1;
+        rear = -1;
     }
 
-    // check if the queue is full
-    public boolean isFull() {
+    // Method to check if the queue is full
+    boolean isFull() {
+        if (rear == size - 1) {
+            return true;
+        }
         return false;
     }
 
-    // check if the queue is empty
-    public boolean isEmpty() {
+    // Method to check if the queue is empty
+    boolean isEmpty() {
+        if (front == -1 || front > rear) {
+            return true;
+        }
         return false;
     }
 
-    // insert elements to the queue
-    public void enqueue(int element) {
-
+    // Method to enqueue an element into the queue
+    void enqueue(int element) {
+        // If the queue is full
+        if (isFull()) {
+            System.out.println("Queue is full!");
+        } else {
+            if (front == -1) {
+                // When an element is inserted, set the "front" index to 0
+                front = 0;
+            }
+            rear++;
+                // Insert the element at the end
+            items[rear] = element;
+            System.out.println("Insert " + element);
+        }
     }
 
-    // delete element from the queue
-    public int dequeue() {
-        return 0;
+    // Method to dequeue an element from the queue
+    int dequeue() {
+        int element;
+
+        // If the queue is empty
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return (-1);
+        } else {
+            // Delete the first element
+            element = items[front];
+
+            // If there is only one element in the queue
+            if (front == rear) {
+                front = -1;
+                rear = -1;
+            } else {
+                // Set the next element as the new "front"
+                front++;
+            }
+            System.out.println(element + " deleted");
+            return element;
+        }
     }
 
-    public int getFront() {
-        return front;
-    }
+    // Method to display the elements in the queue
+    void display() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+        } else {
+            // Display the first element
+            System.out.println("\nFront Index -> " + front);
 
-    public int getRear() {
-        return rear;
-    }
+            // Display all the remaining elements in the queue
+            System.out.println("Items -> ");
+            for (int i = front; i <= rear; i++) {
+                System.out.print(items[i] + " ");
+            }
 
-    @Override
-    public String toString() {
-        return "MyQueue{}";
+            // Display the last element
+            System.out.println("\nRear Index -> " + rear);
+        }
     }
-
 }
-
-
--- src/QueueExample.java
+++ src/QueueExample.java
@@ -1,19 +1,26 @@
-public class Main {
+import java.util.Queue;
+
+public class QueueExample {
     public static void main(String[] args) {
-        // create an object of Queue class
+        // create an object of MyQueue class
+        MyQueue a = new MyQueue();
 
         // try to delete element from the queue
-
         // currently queue is empty
-
         // so deletion is not possible
+        a.dequeue();
 
         // insert elements to the queue
+        for (int i = 1; i <= a.size; i++) {
+            a.enqueue(i);
+        }
 
         // check when queue is full
+        a.enqueue(10);
 
         // removes element until it is empty
-
+        a.dequeue();
         // check if queue is empty
+        a.display();
     }
 }
